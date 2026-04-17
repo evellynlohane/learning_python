44fb080b58dab1fc8fa4ee1ef78e3aaa55ecc34a
evellynlohane <77evellynlohane@gmail.com>
2026-04-17 20:10:52 -0300

    Atividade_9_4

--- Atividade_9_4.py
+++ Atividade_9_4.py
@@ -18,9 +18,11 @@ print('Fim')
 ###########
 print('                ')
 
+print('Insira 10 idades: ')
+
 for idade in range (0,11,1):
-    print('Insira 10 idades: ')
-    idade = int(input('Digite uma idade (-1 para sair):'))
+    print('                ') #Utilizei por estética
+    idade = int(input('Digite uma idade:'))
     if idade > 17:
         print(f'Maior de idade: {idade}')
     else:
@@ -29,4 +31,15 @@ for idade in range (0,11,1):
 ###########
 print('                ')
 
+print('Insira 30 idades: ')
+print('                ') #Utilizei por estética
 
+for nota in range (0,31,1):
+    print('                ') #Utilizei por estética
+    nota = int(input('Digite uma nota para portugês:'))
+    if nota >= 7:
+        print(f'Aprovado: {nota}')
+    elif nota < 7 and nota >= 5:
+        print(f'Em recuperação: {nota}')
+    else:
+        print(f'Reprovado: {nota}')
